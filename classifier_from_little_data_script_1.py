9521c63349eba069a640e70972cc8dc4a046cff0
Jaeho Lee <owen12123@gmail.com>
2017-06-22 21:10:32 -0400

    added layers

--- classifier_from_little_data_script_1.py
+++ classifier_from_little_data_script_1.py
@@ -72,8 +72,16 @@ model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(128, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
 model.add(Flatten())
-model.add(Dense(64))
+model.add(Dense(128))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
 model.add(Dense(1))
